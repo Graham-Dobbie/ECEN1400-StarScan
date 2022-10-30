e110f327e3125302cf74c83d6eac246aef3c2cc2
Graham Dobbie <grahamabq@gmail.com>
2022-10-30 17:45:12 -0600

    User class working

--- User/user.cpp
+++ User/user.cpp
@@ -2,177 +2,202 @@
 
 User::User() : student_name_("NULL"), points_(-1), student_name_max_length__(16) {}
 
-User::User(std::string name, std::string uid) : student_name_(name), points_(0), student_name_max_length__(16){
-    
+User::User(std::string name, std::string uid) : student_name_(name), points_(0), student_name_max_length__(16) {
+
     int set_uid_code = setUID(uid);
-    if (set_uid_code == 1){
+    if (set_uid_code == 1) {
         std::cout << "UID incorect" << std::endl;
     }
-    
-    
 }
 
-std::string User::getUsername(){
-    
-    return student_name_;
-}
+std::string User::getUsername() { return student_name_; }
 
-int User::setUsername(std::string name){
-    
-    if( name.length() > student_name_max_length__) {
-        
-        student_name_ = name.substr(0 , 15);
+int User::setUsername(std::string name) {
+
+    if (name.length() > student_name_max_length__) {
+
+        student_name_ = name.substr(0, 15);
         return 1;
-        
-    }else {
-        
+
+    } else {
+
         student_name_ = name;
         return 0;
     }
 }
 
+std::string User::getUID() {
 
-
-
-std::string User::getUID(){
-    
     std::string return_string;
-    
-    for(int i = 0; i < 4; i++){
-        
+
+    for (int i = 0; i < 4; i++) {
+
         char hexes[2];
         int id = student_id_[i];
         intToChr(hexes, id);
-        
-        std::cout << id << " " << hexes[0] << hexes[1] << std::endl;
-        
+
         return_string += hexes[0];
         return_string += hexes[1];
         return_string += ' ';
     }
-    
-    return return_string.substr(0,10);
+
+    return return_string.substr(0, 11);
 }
 
+int User::setUID(std::string uid) {
 
-int User::setUID(std::string uid){
-    
     std::string hexes[4];
-    
-    
+
     int current_hex = 0;
-    for(int i = 0; i < uid.length(); i++){
-        
-        if(uid[i] == ' '){
-            
-            if(current_hex <= 3){
+    for (int i = 0; i < uid.length(); i++) {
+
+        if (uid[i] == ' ') {
+
+            if (current_hex <= 3) {
                 current_hex++;
-                
-                
-            } else{
+
+            } else {
                 return 1;
             }
-            
-        }
-        else{
+
+        } else {
             hexes[current_hex] += uid[i];
         }
     }
-    
-    for(int i = 0; i < 4; i++){
-        int i_hex = 16*chrToInt(hexes[i][0]) +chrToInt(hexes[i][1]);
+
+    for (int i = 0; i < 4; i++) {
+        int i_hex = 16 * chrToInt(hexes[i][0]) + chrToInt(hexes[i][1]);
+        student_id_[i] = i_hex;
     }
-    
+
     return 0;
 }
-    
-    
 
-// std::string User::getUserDataByElement(std::string element);
-// int User::writeUserDataByElement( std::string element, std::string value);
-
-int User::givePoint(){
-    points_ ++;
+int User::givePoint() {
+    givePoints(1);
     return 0;
-    
 }
-int User::givePoints( int points){
+int User::givePoints(int points) {
     points_ += points;
     return 0;
-    
 }
-int User::setPoints(int points){
+int User::setPoints(int points) {
     points_ = points;
     return 0;
 }
 
-int User::getPoints(){
-    return points_;
+int User::getPoints() { return points_; }
+
+int User::loadUser(std::string load_string) { // uid1,uid2,uid3,uid4,name,points
+
+    std::string components[10];
+    int numb_splits = split(load_string, components, 10);
+
+    if (numb_splits < 0) {
+        return 1;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        student_id_[i] = std::stoi(components[i]);
+    }
+
+    setUsername(components[4]);
+    setPoints(stoi(components[5]));
+
+    return 0;
 }
 
+std::string User::saveUser() { // uid1,uid2,uid3,uid4,name,points
 
-int User::intToChr(char chrs[2], int convert_int){
+    std::string data;
+
+    for (int id : student_id_) {
+        data += std::to_string(id);
+        data += ",";
+    }
+
+    data += student_name_;
+    data += ",";
+
+    data += std::to_string(points_);
+
+    return data;
+}
+
+int User::intToChr(char chrs[2], int convert_int) {
     int h[2];
-    if (convert_int < 256){
-        
+    if (convert_int < 256) {
+
         h[1] = convert_int % 16;
-        h[0] = (convert_int - h[1])/16;
-        
-        for(int i = 0; i < 2; i++){
-            if(h[i] == 10){
+        h[0] = (convert_int - h[1]) / 16;
+
+        for (int i = 0; i < 2; i++) {
+            if (h[i] == 10) {
                 chrs[i] = 'A';
-            }
-            else if(h[i] == 11){
+            } else if (h[i] == 11) {
                 chrs[i] = 'B';
-            }
-            else if(h[i] == 12){
+            } else if (h[i] == 12) {
                 chrs[i] = 'C';
-            }
-            else if(h[i] == 13){
+            } else if (h[i] == 13) {
                 chrs[i] = 'D';
-            }
-            else if(h[i] == 14){
+            } else if (h[i] == 14) {
                 chrs[i] = 'E';
-            }
-            else if(h[i] == 15){
+            } else if (h[i] == 15) {
                 chrs[i] = 'F';
-            }
-            else{
+            } else {
                 chrs[i] = char(48 + h[i]);
-            }   
+            }
         }
-        
+
         return 0;
     }
-    
-    else{
-        
+
+    else {
+
         return -1;
     }
 }
 
+int User::chrToInt(char hex_value) {
 
-int User::chrToInt(char hex_value){
-    
-    if(hex_value == 'A'){
+    if (hex_value == 'A') {
         return 10;
-    }
-    else if(hex_value == 'B'){
+    } else if (hex_value == 'B') {
         return 11;
-    }
-    else if(hex_value == 'C'){
+    } else if (hex_value == 'C') {
         return 12;
-    }
-    else if(hex_value == 'D'){
+    } else if (hex_value == 'D') {
         return 13;
-    }
-    else if(hex_value == 'E'){
+    } else if (hex_value == 'E') {
         return 14;
-    }
-    else if(hex_value == 'F'){
+    } else if (hex_value == 'F') {
         return 15;
-    }
-    else{
+    } else {
         return hex_value - '0';
+    }
+}
+
+int User::split(std::string line, std::string comps[], int max_size) {
+
+    int err_code = 0;
+    int comp_pos = 0;
+
+    for (int pos = 0; (pos < line.length()) and (err_code == 0); pos++) {
+        if (line[pos] == ',') {
+
+            comp_pos++;
+
+            if (comp_pos >= max_size) {
+                err_code = -1;
+            }
+        } else {
+            comps[comp_pos] += line[pos];
+        }
+    }
+
+    if (err_code != 0) {
+        return err_code;
+    } else {
+        return comp_pos + 1;
     }
 }
--- User/user.hpp
+++ User/user.hpp
@@ -1,17 +1,16 @@
 #ifndef User_h
 #define User_h
 
-#include <string>
-#include <map>
-#include <cstddef>
 #include <bitset>
+#include <cstddef>
 #include <iostream>
+#include <map>
+#include <string>
 
-class User
-{
+class User {
   public:
     User();
-    User(std::string name , std::string uid);
+    User(std::string name, std::string uid);
 
     std::string getUsername();
     int setUsername(std::string name);
@@ -20,28 +19,30 @@ class User
     int setUID(std::string uid);
 
     std::string getUserDataByElement(std::string element);
-    int writeUserDataByElement( std::string element, std::string value);
+    int writeUserDataByElement(std::string element, std::string value);
 
     int givePoint();
-    int givePoints( int points);
+    int givePoints(int points);
     int setPoints(int points);
 
     int getPoints();
-    
+
+    int loadUser(std::string load_string);
+    std::string saveUser();
+
     int chrToInt(char hex);
     int intToChr(char chrs[2], int convert_int);
 
+    int split(std::string line, std::string comps[], int max_size);
+
   private:
-  
-    
     std::string student_name_;
     int student_name_max_length__;
     int student_id_[4];
 
     int points_;
 
-    std::map< std::string , std::string> user_data_;
-
+    std::map<std::string, std::string> user_data_;
 };
 
 #endif
